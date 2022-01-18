82bfc7ccd3e7baa5eb8614a482805656ecfbe90b
Oguzhan <fgtyhtyh@hotmail.com>
2022-01-18 15:13:11 +0300

    prepare api for key generation and registration

--- api/models/User.js
+++ api/models/User.js
@@ -1,10 +1,16 @@
 const mongoose = require("mongoose");
+const { nanoid } = require('nanoid')
 
 const UserSchema = new mongoose.Schema(
   {
+    _id: {
+      type: String,
+      default: () => nanoid(12)
+    },
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    publicKey: {type: String, required: true},
     isAdmin: { type: Boolean, default: false },
     profilePicture: { type: String, default: "" },
     friends: { type: Array, default: [] },
